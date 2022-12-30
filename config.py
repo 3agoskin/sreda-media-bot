a68dfbd9ed054f96353ad56842db9b386e61a0a7
KirillZagoskin <kirillzagoskin.work@gmail.com>
2022-12-30 08:35:14 +0300

    commit 2

--- config.py
+++ config.py
@@ -4,4 +4,12 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = os.getenv("")
+TOKEN = os.getenv("TG_BOT_API_TOKEN")
+BOT_VERSION = 0.1
+
+BOT_DB_NAME = "survey_new_year_tree"
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+REDIS_PASSWORD = None
